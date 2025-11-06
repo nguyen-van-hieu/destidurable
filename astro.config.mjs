caf532cd9c7cfe59b5e7896d39e2570a164985c7
Hieu Nguyen <hieu.nguyen@mettadepth.com>
2025-11-06 09:35:37 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import compressor from "astro-compressor";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://destidurable.com",
+  image: {
+    service: { entrypoint: 'astro/assets/services/noop' }
+  },
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap(),
+    compressor({ gzip: true, brotli: false }),
+  ],
+});
